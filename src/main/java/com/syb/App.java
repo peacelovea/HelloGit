f28e4eef722e379224c223afa8906d6c61592506
erhang <88267368+peacelovea@users.noreply.github.com>
2022-03-19 15:04:02 +0800

    pull commit

--- src/main/java/com/syb/App.java
+++ src/main/java/com/syb/App.java
@@ -16,5 +16,6 @@ public class App
         System.out.println( "confilct! hot-fix-test" );
         System.out.println( "push test!" );
         System.out.println( "ssh push test!" );
+        System.out.println( "pull test!" );
     }
 }
